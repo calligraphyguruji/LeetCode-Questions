e20c3236b629b845349033657f6ec4cbccf9e04b
Aman Mishra <hiaman4046@gmail.com>
2026-08-19 20:51:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.62%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    //Time-Complexity = O(log |n|)
+    //Space Complexity = O(1)
+    double myPow(double x, int n) {
+      if(n == 0) return 1.0 ;
+    
+    if(x == 0) return 0.0 ;
+    
+    if(x == 1) return 1.0 ;
+    
+    if(x == -1){
+        if(n % 2 == 0) 
+           return 1.0 ;
+        
+        else 
+           return -1.0 ;
+    }
+    
+    
+    long binForm = n ;
+    double ans = 1;
+    
+    if(n < 0){
+        x = 1/x ;
+        binForm = -binForm ;
+    }
+    
+    
+  
+    while(binForm > 0){
+        if(binForm % 2 == 1){
+            ans *= x;
+        }
+        
+        x *= x;
+        binForm /= 2;
+    }
+    
+    return ans ;  
+    }
+};

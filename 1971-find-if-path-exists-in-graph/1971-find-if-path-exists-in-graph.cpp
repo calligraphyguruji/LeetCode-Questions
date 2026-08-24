61789d4fbe0e9fffb9f0723b8a3d383332558961
Aman Mishra <hiaman4046@gmail.com>
2026-08-24 22:49:52 +0530

    Time: 544 ms (11.18%), Space: 345.9 MB (10.91%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    bool DFS(unordered_map<int, vector<int>>& map, int src, int dst, vector<bool>& visited){
+        //edge-case/base-case
+        if(src == dst) return true;
+
+        //mark source in visited true
+        visited[src] = true;
+
+        //traverse in neighbors of source
+        for(auto& node : map[src]){
+            if(!visited[node]){ //not visited then visit
+                if(DFS(map, node, dst, visited) == true){//means path exits
+                   return true; 
+            }
+            }
+            
+        }
+
+        //if in above checks no path found
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int src, int dst) {
+        //1.) Build graph(using unordered_map Adjacency list)
+        unordered_map<int, vector<int>> map ;
+
+        for(auto& edge : edges){
+            int u = edge[0] ;
+            int v = edge[1] ;
+
+            //birectional graph : 
+            map[u].push_back(v);//u--->v
+            map[v].push_back(u); //v--->u
+        }
+
+        //visited bool array to store if visited or not
+        vector<bool>visited(n, false) ;
+
+        //func. call to DFS
+        return DFS(map, src, dst, visited);
+    }
+};

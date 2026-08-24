ca0fb16e3d62d1ebce660b9d794244b392a370e9
Aman Mishra <hiaman4046@gmail.com>
2026-08-24 23:00:35 +0530

    Time: 402 ms (35.19%), Space: 329.1 MB (23.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,30 +1,10 @@
 class Solution {
 public:
-    //Method-1: DFS(Depth First Search)
+    //Method-2: BFS(Breadth First Search)
     
     //Time Complexity = O(V + E) => V = vertices(nodes), edges = E
     //Space Complexity = O(V + E)
 
-    bool DFS(unordered_map<int, vector<int>>& map, int src, int dst, vector<bool>& visited){
-        //edge-case/base-case
-        if(src == dst) return true;
-
-        //mark source in visited true
-        visited[src] = true;
-
-        //traverse in neighbors of source
-        for(auto& node : map[src]){
-            if(!visited[node]){ //not visited then visit
-                if(DFS(map, node, dst, visited) == true){//means path exits
-                   return true; 
-            }
-            }
-            
-        }
-
-        //if in above checks no path found
-        return false;
-    }
     bool validPath(int n, vector<vector<int>>& edges, int src, int dst) {
         //1.) Build graph(using unordered_map Adjacency list)
         unordered_map<int, vector<int>> map ;
@@ -41,7 +21,28 @@ public:
         //visited bool array to store if visited or not
         vector<bool>visited(n, false) ;
 
-        //func. call to DFS
-        return DFS(map, src, dst, visited);
+        queue<int> q;
+
+        //start BFS from src node
+        q.push(src);
+        visited[src] = true;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            if(node == dst) return true;
+
+            //else : traverse in neighbors of node
+            for(auto& V : map[node]){
+                if(!visited[V]){
+                    q.push(V);
+                    visited[V] = true;
+                }
+            }
+        }
+        
+        //if in above BFS checks no path found
+        return false;
     }
 };

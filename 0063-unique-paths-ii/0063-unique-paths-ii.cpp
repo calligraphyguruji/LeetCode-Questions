d99f3097af0443f89f3cd001b5a3d623531e25b4
Aman Mishra <hiaman4046@gmail.com>
2026-08-25 22:56:02 +0530

    Time: 0 ms (100%), Space: 11.2 MB (98.8%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,60 +1,47 @@
 class Solution {
 public:
-    //Method-2 : Bottom-up Approach
-    
+    //Method-1 : Top-Down Approach (Recursion + Memoization)
+
     //Time Complexity = O(m * n)
     //Space Complexity = O(m * n)
+
+    int m , n ; //global m, n so that we don't have to pass in parameters of solve function.
     
+    int dp[101][101] ;//memoization dp table
+
+    int solve(int i, int j, vector<vector<int>>& grid){
+        //base-case : check out of bound and avoid obstacle
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 1){
+            return 0 ;
+        }
+
+        //if reached to destination return 1 path found
+        if(i == m-1 && j == n-1){
+            return 1 ;
+        }
+
+
+        //check memoization table : 
+        if(dp[i][j] != -1){ //means we have solved before
+            return dp[i][j] ; //don't solve again just return the solved
+        }
+
+        //choose options :
+        int right = solve(i, j+1, grid) ;
+        int down = solve(i+1, j, grid) ;
+
+        //return total no. of ways
+        return dp[i][j] = right + down ;
+
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         
-        int m = grid.size() ;
-        int n = grid[0].size() ;
+        memset(dp, -1, sizeof(dp)) ; //initialize dp table with -1
+        
+        m = grid.size(); //rows
+        n = grid[0].size() ; //columns
+        
+        return solve(0, 0, grid) ;
 
-        vector<vector<int>> dp(m, vector<int>(n)) ;
-
-        dp[0][0] = 1 ;//given in testcase if src == dst then 1 way to reach
-
-        //fill 0th row in dp
-        for(int j=0 ; j<n ; j++){
-            if(j > 0 && grid[0][j-1] == 1){ //check in left if obstacle found
-                dp[0][j] = 0 ;
-                grid[0][j] = 1 ; //update grid obstacle so that we can't reach any cell in the right
-            }
-            else if(grid[0][j] == 1){
-                dp[0][j] = 0 ;
-            }
-            else{ //if no obstacle then 
-                dp[0][j] = 1 ; // 1 path found
-            }
-        }
-
-        //fill 0th column in dp
-        for(int i=0 ; i<m ; i++){
-            if(i > 0 && grid[i-1][0] == 1){ //check in upside if obstacle found
-                dp[i][0] = 0 ;
-                grid[i][0] = 1 ; //update grid obstacle so that we can't reach any cell in the downside
-            }
-            else if(grid[i][0] == 1){
-                dp[i][0] = 0 ;
-            }
-            else{ //if no obstacle then
-                dp[i][0] = 1 ; // 1 path found
-            }
-        }
-
-        //fill remaining cells in dp
-        for(int i=1 ; i<m ; i++){
-            for(int j=1 ; j<n ; j++){
-                if(grid[i][j] == 1){
-                    dp[i][j] = 0 ;
-                }
-                else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1] ;
-                }
-            }
-        }
-
-        //return total no. of ways to reach [m-1][n-1]
-        return dp[m-1][n-1] ;
     }
 };

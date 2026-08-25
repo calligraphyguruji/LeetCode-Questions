4a785baf985139ca8ea3fa307c7f275815f5d5f4
Aman Mishra <hiaman4046@gmail.com>
2026-08-25 22:52:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (42.64%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    //Method-3 : Catalan Number
+    //Method-2 : Bottoum-Up Approach
 
     //Time Complexity = O(n)
-    //Space Complexity = O(1)
-     
-    int catalan(int n) {
+    //Space Complexity = O(n)
 
-        long long ans = 1;
+    int numTrees(int n) {
+        //make dp array
+        vector<int>dp(n+1, 0) ; //initialize with 0
 
-        for(int i = 0; i < n; i++) {
-            ans = ans * (2 * i + 1) * 2 / (i + 2);
+        //base-case :
+        dp[0] = dp[1] = 1 ; // for 1 node or 0 node , ans = 1
+
+        //fill remaining 
+        // i = no. nodes = n
+        for(int i=2 ; i<= n ; i++){
+             //choose root for every i
+             for(int j=1 ; j<= i ; j++){
+                dp[i] += dp[j-1]* dp[i-j] ;
+             }
         }
 
-        return ans;
-
-    }
-    int numTrees(int n) {
-        
-        //func. call to find nth catalan
-        return catalan(n) ;
+        //return dp[n] = no. of unique BSTs for n nodes
+        return dp[n] ;
     }
 };

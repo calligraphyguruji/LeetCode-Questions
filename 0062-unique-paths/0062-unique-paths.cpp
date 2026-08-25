79440b1b9745ac75e4d1ced5792283570730f5fb
Aman Mishra <hiaman4046@gmail.com>
2026-08-25 22:55:40 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    //Method-2 : Bottom-Up Approach
-    //Time Complexity = O(m * n)
-    //Space Complexity = O(m * n)
-    
+    int solve(int i, int j, int m, int n, vector<vector<int>>& dp){
+        //base-case
+        if(i < 0 || i >= m || j < 0 || j >= n){ //out of bound
+            return 0 ;
+        }
+
+        //if reached to destination return 1 path found
+        if(i == m-1 && j == n-1){
+            return 1 ;
+        }
+        
+        //check memoization table
+        if(dp[i][j] != -1){ //means solved before 
+            return dp[i][j] ; //just return that state don't solve again
+        }
+        int right = solve(i, j+1, m, n, dp) ;
+        int down = solve(i+1, j, m, n, dp) ;
+
+        return dp[i][j] = right + down ;
+
+
+    }
     int uniquePaths(int m, int n) {
         
-        //2d dp vector (m x n size)
-        vector<vector<int>> dp(m, vector<int>(n)) ;
-        
-        dp[0][0] = 1 ; //assumption : source cell => source cell given = 1
+        //2d dp vector
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1)) ;
 
-        //base-case :
-        //fill 0th row
-        for(int j = 1 ; j < n ; j++){
-            dp[0][j] = 1 ;
-        }
 
-        //fill 0th col
-        for(int i=1 ; i < m ; i++){
-            dp[i][0] = 1 ;
-        }
-        
-
-        //fill remaining cells of dp
-
-        for(int i= 1 ; i<m ; i++){
-            for(int j=1 ; j<n ; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] ;
-            }
-        }
-
-        //destination cell
-        return dp[m-1][n-1] ;
+        return solve(0, 0, m, n, dp) ;
     }
 };

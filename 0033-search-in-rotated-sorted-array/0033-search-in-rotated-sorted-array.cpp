c63840b5c4192f04eb81e4c47e8b9294022411bb
Aman Mishra <hiaman4046@gmail.com>
2026-08-26 23:42:37 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.81%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    //Method : Modified Binary Search
+
+    //Time Complexity = O(logn)
+
+    //Space Complexity = O(1)
+    int search(vector<int>& nums, int target) {
+      int n = nums.size() ;
+    int st = 0, end = n-1 ;
+    
+    while(st <= end){
+        
+        int mid = st + (end-st)/2 ;
+        
+        if(nums[mid] == target){
+            return mid;
+        }
+        
+        if(nums[st] <= nums[mid]){ //left sorted
+            
+            if(nums[st] <= target && target <= nums[mid]){
+                
+                end = mid - 1 ;
+            } 
+            else {
+                st = mid + 1 ;
+            }
+            
+        } 
+        else {    //right sorted
+           
+           if(nums[mid] <= target && target <= nums[end]){
+               
+               st = mid + 1;
+           } 
+            else {
+                end = mid - 1 ;
+            }
+        }
+        
+        
+        
+    }  
+      return -1 ;
+    }
+};

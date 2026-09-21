c105fc6e92e72ee8cdabc0fb9bba598a9ad3596f
Aman Mishra <hiaman4046@gmail.com>
2026-09-21 23:22:38 +0530

    Time: 0 ms (100%), Space: 8 MB (68.58%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    //Why God Why ?? this is a leetcode problem??? 
+    //NO God please!!!!!!
+    
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
